cb982acf7cb3c5db7e628b5c807ab20f65d18727
Rob Ballou <rob@atendesigngroup.com>
2013-12-05 10:25:11 -0700

    Added new git subcommand: edit. Opens the git folder in your EDITOR

--- context_commands/git.py
+++ context_commands/git.py
@@ -9,5 +9,7 @@ class Git(Command):
         git_directory = os.path.expanduser(context['git'])
         if not args.subcommand:
             print "cd %s" % git_directory
+        elif args.subcommand and args.subcommand[0] == 'edit':
+            print "$EDITOR %s" % git_directory
         else:
             print "pushd %s && git %s && popd" % (git_directory, " ".join(args.subcommand))
