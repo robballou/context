3c3326579f28ea2df18d67f657d63d05b09e38a1
Rob Ballou <rob@atendesigngroup.com>
2013-12-05 10:29:09 -0700

    Adding new web subcommand: edit. Opens the web folder in your EDITOR

--- context_commands/web.py
+++ context_commands/web.py
@@ -1,6 +1,18 @@
+import os
 from context_commands import Command
 
 class Web(Command):
     alias = 'w'
+
     def default(self, context, args):
-        print "cd %s" % context['web']
+        print "cd %s" % os.path.expanduser(context['web'])
+        return True
+
+    def run(self, context, args):
+        return_value = super(Web, self).run(context, args)
+        if return_value:
+            return
+
+        if args.subcommand and args.subcommand[0] == 'edit':
+            print "$EDITOR %s" % os.path.expanduser(context['web'])
+
