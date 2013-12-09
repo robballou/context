b81b13612e2599b70d0faa41379773fabd12ccc8
Rob Ballou <rob@atendesigngroup.com>
2013-12-09 13:12:49 -0700

    Default for links now displays the links for a context

--- context_commands/links.py
+++ context_commands/links.py
@@ -4,7 +4,11 @@ class Links(Command):
     alias = 'l'
     def run(self, context, args, contexts):
         if not args.subcommand:
-            print "echo Please enter a link name: context link name"
+            self.error_message("Links:")
+            links = context['links'].keys()
+            links.sort()
+            for link in links:
+                self.error_message("\t%s" % link)
         elif args.subcommand[0] in context['links']:
             print "open %s" % context['links'][args.subcommand[0]]
         else:
