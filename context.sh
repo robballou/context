b24988f94361046b4ed098dc08f48e1a36b1ea9c
Rob Ballou <rob@atendesigngroup.com>
2014-05-12 11:24:46 -0600

    Updating context.sh for zsh

--- context.sh
+++ context.sh
@@ -3,8 +3,21 @@
 #
 # Pass the commands to the python script and back to the shell
 #
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-response=$(python $DIR/context.py "$@")
-if [[ $response ]]; then
-	eval $response
+DIR="empty"
+
+if [[ -n "$BASH_SOURCE" ]]; then
+	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+fi
+
+if [[ -n "$CONTEXT_HOME" ]]; then
+	DIR=$CONTEXT_HOME
+fi
+
+if [[ "$DIR" = "empty" ]]; then
+	echo "Cannot find context. Please set CONTEXT_HOME"
+else
+	response=$(python $DIR/context.py "$@")
+	if [[ -n "$response" ]]; then
+		eval $response
+	fi
 fi
