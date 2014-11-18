8a78ca3c1c71ab6b089ad1c92526757a65731c1c
Rob Ballou <rob.ballou@gmail.com>
2014-11-17 19:01:38 -0700

    Added a subcommand for contexts command: contexts [context name] now show details of a context

--- context/commands/contrib/contexts.py
+++ context/commands/contrib/contexts.py
@@ -1,7 +1,10 @@
+import pprint
+import sys
+
 from context.commands import Command
 
 class Contexts(Command):
-    """List loaded contexts"""
+    """List loaded contexts and view information about a context"""
     def default(self, context, args, contexts):
         self.error_message("Contexts:")
         context_keys = []
@@ -15,3 +18,13 @@ class Contexts(Command):
         context_keys.sort()
         for context_key in context_keys:
             self.error_message("\t%s" % context_key)
+
+    def run(self, context, args, contexts):
+        # details about a specific context
+        if args.subcommand:
+            this_context = args.subcommand[0]
+            pp = pprint.PrettyPrinter(indent=4, stream=sys.stderr)
+            pp.pprint(contexts.contexts[this_context])
+            return
+
+        self.default(context, args, contexts)
