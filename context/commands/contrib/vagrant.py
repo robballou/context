c9c0fc082d309064fd5c77c00cc7f5e8dfc9ec1d
Rob Ballou <rob.ballou@gmail.com>
2016-08-17 09:50:07 -0600

    Support passing ssh commands to vagrant

--- context/commands/contrib/vagrant.py
+++ context/commands/contrib/vagrant.py
@@ -13,5 +13,12 @@ class Vagrant(CommandPasser):
             print "cd %s" % vagrant_directory
         elif args.subcommand[0] == 'down':
             print self.make_command_context_specific('vagrant halt', vagrant_directory)
+        elif args.subcommand[0] == 'ssh' and len(args.subcommand) > 1:
+            # inject --command before the subsequent commands
+            args.subcommand.insert(1, '--command')
+            # make sure to quote the command
+            args.subcommand[2] = '"%s' % args.subcommand[2]
+            args.subcommand[-1] = '%s"' % args.subcommand[-1]
+            super(Vagrant, self).run(context, args, contexts)
         else:
             super(Vagrant, self).run(context, args, contexts)
